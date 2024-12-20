965b39936f664850ec6e201c6f6adbe85165aa07
Mohammad Farhan <mohammadfarhan@MFARHAN-PAT-IN.local>
2024-12-21 01:21:23 +0530

    added portfolio code

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//preloader -------------------------------------
+const loader = document.getElementById('preloader');
+
+window.onload = function () {
+    loader.classList.add('loaderpre');
+}
+
+//preloader -------------------------------------
+
+//Text loader --------------------------------------
+const typed = new Typed('.multiple_text',{
+    strings: ['Dedicated', 'Passionate'],
+    typeSpeed: 80,
+    backSpeed:80,
+    backDelay:1200,
+    loop: true,
+
+  });
+
+//Text loader --------------------------------------
